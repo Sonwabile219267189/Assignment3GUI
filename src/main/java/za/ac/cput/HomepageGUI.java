d52e4963c899e82bcd735d70e27934abeb76c24a
Sonwabile <219267189@mycput.ac.za>
2021-10-20 23:24:02 +0100

    Added homepage

---
+++ src/main/java/za/ac/cput/HomepageGUI.java
@@ -0,0 +1,18 @@
+package za.ac.cput;
+
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
+
+public class HomepageGUI {
+
+    private JFrame homeFrame;
+    private JPanel panelNorth, panelWest, panelEast;
+    private JLabel lblHeading;
+    private JButton btnCreatePrescription, btnAddPatient;
+
+    public HomepageGUI(){
+        homeFrame = new JFrame("Select Position");
+
+    }
+}
